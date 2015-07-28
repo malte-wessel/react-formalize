6e90ad3b1b5eeadf05a503b23c81f61fc5266b7a
Malte W <wessel@rheinschafe.de>
2015-07-28 12:15:29 +0200

    Added select input

---
+++ src/components/inputs/Select.js
@@ -0,0 +1,37 @@
+import React, { PropTypes } from 'react';
+import Input from '../Input';
+
+export default class Select extends React.Component {
+
+    static propTypes = {
+        children: PropTypes.oneOfType([
+            PropTypes.element,
+            PropTypes.arrayOf(PropTypes.element)
+        ])
+    }
+
+    static defaultProps = {
+        children: {}
+    }
+
+    serialize(e) {
+        // target is undefined in react@0.14.0-beta1
+        // see https://github.com/facebook/react/issues/4288
+        const target = e.target || e.currentTarget;
+        const { value } = target;
+        return value;
+    }
+
+    render() {
+        const {children, ...props} = this.props;
+        return (
+            <Input serialize={this.serialize} {...props}>
+                {sprops => {
+                    return (
+                        <select {...sprops}>{children}</select>
+                    );
+                }}
+            </Input>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import Input from './components/Input';
 
 import Text from './components/inputs/Text';
 import Checkbox from './components/inputs/Checkbox';
+import Select from './components/inputs/Select';
 
 export default {
     Form,
     Input,
 
     Text,
-    Checkbox
+    Checkbox,
+    Select
 };
