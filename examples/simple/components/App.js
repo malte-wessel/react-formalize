48989bd611623797b42ade243021ece2bfa61fcf
Malte W <wessel@rheinschafe.de>
2015-07-28 14:30:35 +0200

    Updated examples

--- examples/simple/components/App.js
+++ examples/simple/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, Text, Checkbox, Select } from 'react-formalize';
+import { Form, Text, Checkbox, Select, RadioGroup } from 'react-formalize';
 
 const mock = {
     title: 'Hello World'
@@ -25,15 +25,26 @@ export default class App extends Component {
                         <Text name="description"/>
                     </div>
                     <div>
+                        <label>Category</label>
+                        <Select name="category">
+                            <option value="news">News</option>
+                            <option value="sport">Sport</option>
+                        </Select>
+                    </div>
+                    <div>
                         <label>Publish</label>
                         <Checkbox name="publish"/> publish this post
                     </div>
                     <div>
-                        <label>Category</label>
-                        <Select name="category">
-                            <option value="news">News</option>
-                            <option value="foo">Foo</option>
-                        </Select>
+                        <label>Public</label>
+                        <RadioGroup name="public">
+                            {Radio => (
+                                <div>
+                                    <Radio value="true"/> public
+                                    <Radio value="false"/> private
+                                </div>
+                            )}
+                        </RadioGroup>
                     </div>
                     <div>
                         <button type="submit">Submit</button>
