b6252e9de02289d6abae9a9ec4ba06b03db1fd6f
Malte W <wessel@rheinschafe.de>
2015-07-29 10:04:46 +0200

    support multiple select

--- src/components/inputs/Select.js
+++ src/components/inputs/Select.js
@@ -19,14 +19,32 @@ export default class Select extends Component {
         // target is undefined in react@0.14.0-beta1
         // see https://github.com/facebook/react/issues/4288
         const target = e.target || e.currentTarget;
-        const { value } = target;
+        const { value, type } = target;
+
+        if (type === 'select-multiple') {
+            const values = [];
+            const { options } = target;
+            for (let i = 0, l = options.length; i < l; i++) {
+                const option = options[i];
+                if (option.selected) values.push(option.value);
+            }
+            return values;
+        }
+
         return value;
     }
 
     render() {
-        const {children, ...props} = this.props;
+        const {children, value, multiple, ...props} = this.props;
+        let finalValue;
+
+        if (multiple && !Array.isArray(value)) {
+            if (!value) finalValue = [];
+            else finalValue = [value];
+        }
+
         return (
-            <Input serialize={this.serialize} {...props}>
+            <Input serialize={this.serialize} value={finalValue} multiple={multiple} {...props}>
                 {innerProps => <select {...innerProps}>{children}</select>}
             </Input>
         );
